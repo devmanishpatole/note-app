9e573951bfc0b7ff7c6685e50db6534e0948c386
Manish Patole <manish.patole@team.telstra.com>
2020-11-17 01:44:06 +1100

    Adding npm packages. 1] validator - to perform validation operation on strings. 2] chalk - to print colorful logs.

---
+++ constants.js
@@ -0,0 +1,6 @@
+// Defines all required constants 
+const FILE_NAME = 'notes.txt'
+
+module.exports = {
+    'file_name': FILE_NAME
+}
--- notes.js
+++ notes.js
@@ -1,11 +1,26 @@
+// Dependencies
 const fs = require('fs')
+const constants = require('./constants')
+const validator = require('validator')
+const chalk = require('chalk')
 
-const FILE_NAME = 'notes.txt'
+// Logging themes
+const log = console.log
+const error = chalk.bold.red
+const warning = chalk.keyword('orange')
+const success = chalk.green.bold.inverse
 
+// Adding note to file
 const addNote = function(data) {
-    fs.appendFileSync(FILE_NAME, data)
+    if (!validator.isEmpty(data)) {
+        fs.appendFileSync(constants.file_name, data)
+        log(success('Note added successfully'))
+    } else {
+        log(error('Note data is empty, please enter valid note data'))
+    }
 }
 
+// Exporting methods
 module.exports = {
     'addNote': addNote
 }
